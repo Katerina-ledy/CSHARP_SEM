b5005ccf4f7cdc2ee2b17eb6f9e260241582e477
Alekper <alekper@ya.ru>
2024-11-03 23:27:15 +0300

    Added Sem2_Homework3

--- Sem2_Homework3/Program.cs
+++ Sem2_Homework3/Program.cs
@@ -7,32 +7,26 @@
 // 72 => 7
 // Решение
 
-Console.WriteLine("Введите положительное двузначное число: ");
+
+Console.Write("Введите положительное двузначное число от 10 до 99: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num >= 10 || num <= 99)
+if (num >= 10 && num <= 99)
 {
-    int firstDigit = num / 10;  // 26 / 10 = 2.6 = 2
-    int secondDigit = num % 10;   // 26 % 10 = 2.6 = 6
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
     
-    Console.WriteLine($"Первая цифра числа: {firstDigit}! вторая цифра числа: {secondDigit}!"); 
-}
-else if (firstDigit > secondDigit)
-{
-    int result1 = firstDigit;
-    Console.WriteLine($"Наибольшая цифра числа: {result}!");
-}
-else if (firstDigit < secondDigit)
-{
-    int result2 = secondDigit;
-    Console.WriteLine($"Наибольшая цифра числа: {result2}!");
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine($"Большая цифра в числе {num} - {firstDigit}");
+    }
+    else
+    {
+        Console.WriteLine($"Большая цифра в числе {num} - {secondDigit}");
+    }
 }
 else
 {
     Console.WriteLine("Введено некорректное число!");
 }
 
-// вводим число и проверка на двузначность
-// получить первую цифру затем вторую
-// сравнить эти две цифры
-// получаю результат в виде наибольшей цифры
