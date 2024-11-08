904e6076e0dbef7cdb81fcf117284a50e7f9866a
Alekper <alekper@ya.ru>
2024-11-08 23:46:41 +0300

    Added Seminar3

---
+++ Seminar3/Sem3_Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задание 1. Совместная работа
+// Семинар 3. Массивы
+// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+// int[] array1 = new int[5];
+// int[] array2 = new int[5] { 3, 6, 8, 3, 9 };
+// int[] array3 = new int[] {3, 6, 8, 3, 9};
+
+int[] array = { 1, 3, 8, 19, 3, 8 };
+int num = 20;
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+string result = isExistNum ? "Да" : "Нет"; // Console.WriteLine(isExistNum ? "Да" : "Нет");
+Console.WriteLine(result);
---
+++ Seminar3/Sem3_Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задание 3. Совместная работа
+// Семинар 3. Массивы
+// Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив.
+// Пример
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
+
+int[] array = { 1, 3, 2, 4, 2, 3 };
+int[] arrayResult = new int[array.Length / 2];
+
+int left = 0;
+int right = array.Lenght - 1;
+
+while (left < right)
+{
+    arrayResult[left] = array[left] * array[right];
+    left++;
+    right--;
+}
+
+for (int i = 0; i < arrayResult.Lenght; i++)
+{
+    Console.Write(arrayResult[i] + " ");
+}
+
