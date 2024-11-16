9055a86d16cfdf348e3ba468dddefe429dae13fb
Alekper <alekper@ya.ru>
2024-11-16 10:46:24 +0300

    Added Seminar3

--- Seminar3/Sem3_Homework1/Program.cs
+++ Seminar3/Sem3_Homework1/Program.cs
@@ -5,5 +5,18 @@
 // Программа должна вывести:
 // Присутствует/Не присутствует.
 
+int[] array = {1, 3, 5, 10, 25, 55, 68, 99 };
+int num = 55;
 
+bool isExistNum = false;
 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+string result = isExistNum ? "Присутствует" : "Не Присутствует";
+Console.WriteLine(result);
