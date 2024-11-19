4d0b356786a300efb8a9522abf7984be2b4cf508
Alekper <alekper@ya.ru>
2024-11-19 22:20:16 +0300

    Added Sem5_Homework4

---
+++ Seminar5/Sem5_Homework4/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задание 4: Замена отрицательных элементов в двумерном массиве
+// Описание: Задайте двумерный массив. Замените все отрицательные числа на их
+// абсолютные значения.
+// Пример входных данных:
+// 1 -2 3
+// -4 5 -6
+// 7 -8 9
+// Пример результата:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// Метод создания двумерного массива
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max) // указываем тип возвращаемого значения, тип метода
+{
+    int[,] matrix = new int[rows, colums]; // матрица 3x4
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  // for (int i = 0; i < rows; i++) // for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  //  for (int j = 0; j < colums; j++) //  for (int j = 0; j < 4; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max); // matrix[0,1,2, 0,1,2,3] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+// Метод для вывода двумерного массива
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод замены всех отрицательных чисел на положительные
+int[,] ReplaceNegativeNumbers(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i< matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = -matrix[i, j];
+        }
+    }
+    return newMatrix;
+}
+
+// Основная программа
+int[,] array2d = CreateMatrixRndInt(3, 3, -11, 11);
+PrintMatrix(array2d);
+
+Console.WriteLine("Заменяю все отрицательные числа на положительные:");
+
+int[,] result = ReplaceNegativeNumbers(array2d);
+PrintMatrix(result);
+
+
+
+
+
+
+
+
