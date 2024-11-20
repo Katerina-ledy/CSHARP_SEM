056af1090c9cdcd29240c12f1ab83ff7e6d4a93e
Alekper <alekper@ya.ru>
2024-11-20 14:02:42 +0300

    Added Sem6_Task2

---
+++ Seminar6/Sem6_Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задание 2. Работа в сессионных залах
+// Семинар 6. Массивы и строки
+// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// string str = "Hello";
+// char[] characters = str.ToCnarArray();
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+char[] StringToCharArray(string s)
+{
+    char[] strArray = new char[s.Length];
+    for (int i = 0; i < s.Length; i++)
+    {
+        strArray[i] = s[i];
+    }
+    return strArray;
+}
+
+void PrintArray(char[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите строку");
+string str = Console.ReadLine(); 
+char[] resArray = StringToCharArray(str);
+PrintArray(resArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
