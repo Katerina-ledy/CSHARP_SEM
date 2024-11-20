de6df22c5b584e6c56ed371d67283d9cf10dd60a
Alekper <alekper@ya.ru>
2024-11-20 13:15:06 +0300

    Added Sem6_Task1

---
+++ Seminar6/Sem6_Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задание 1. Совместная работа
+// Семинар 6. Массивы и строки
+// 1.  Задайте массив символов (тип char []). 
+// 2. Создайте строку из символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// char[] chars = new char[] {'a', 'b', 'c', 'd'};
+// string str = new string(char);
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+string CharArrayToString(char[] arr)
+{
+    string res = string.Empty; // ""; - строка пустая 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+char[] array = { 'a', 'b', 'c', 'd', 'e', 'f' };
+string result = CharArrayToString(array);
+Console.WriteLine(result);
+
+
