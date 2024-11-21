aca54b8e0007ee0687bfaed100bca5f991f22390
Alekper <alekper@ya.ru>
2024-11-21 18:06:48 +0300

    Added Sem7_Task3

---
+++ Seminar7/Sem7_Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задание 3. Совместная работа
+// Семинар 7. Рекурсия
+// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// Указание: Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+
+static void CheckPrintConsonants(string txt, int index)
+{
+    string vowels = "aeiouy";
+    if (char.IsAsciiLetter(txt[index]) && !vowels.Contains(txt[index]))
+    {
+        Console.Write($"{txt[index]}");
+    }
+}
+
+void PrintConsonants(string txt, int index = 0)
+{
+    if (index == txt.Length)
+    {
+        return;
+    }
+    CheckPrintConsonants(txt, index);
+    PrintConsonants(txt, index + 1);
+}
+
+Console.WriteLine("Введите строку содержащую латинские буквы: ");
+string text = Console.ReadLine();
+Console.WriteLine();
+PrintConsonants(text);
+
