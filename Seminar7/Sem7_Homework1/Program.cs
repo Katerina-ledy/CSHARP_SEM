cebea300e1be49cad716e06a1f1f80a71bb0fa35
Alekper <alekper@ya.ru>
2024-11-21 22:54:36 +0300

    Added Sem7_Homework1

---
+++ Seminar7/Sem7_Homework1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Вывод натуральных чисел в промежутке от M до N
+// Описание: Напишите программу, которая выведет все натуральные числа в
+// промежутке от M до N. Используйте рекурсию и не используйте циклы.
+// Пример:
+// ● Вход: M = 1, N = 5
+// ● Выход: 1, 2, 3, 4, 5
+// ● Вход: M = 4, N = 8
+// ● Выход: 4, 5, 6, 7, 8
+
+int GetRandomNumInt(int minNum, int maxNum)
+{
+    Random rnd = new Random();
+    int num = rnd.Next(minNum, maxNum);
+    return num;
+}
+
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m},");
+        PrintNaturalNumbers(m + 1, n);
+    }
+    else if (m == n)
+    {
+        Console.Write(n);
+        return;
+    }
+    else
+    {
+        Console.Write($"{m},");
+        PrintNaturalNumbers(m - 1, n);
+    }
+}
+
+int M = GetRandomNumInt(0, 10);
+int N = GetRandomNumInt(0, 10);
+
+Console.Write($"M = {M}; N = {N}; => ");
+PrintNaturalNumbers(M, N);
+
+
